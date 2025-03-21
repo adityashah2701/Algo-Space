9c5100eb457bb5a2a0fca7a52604661866f55029
adityashah2701 <adityashah2701@gmail.com>
2025-03-21 11:34:35 +0530

    user schema created !

--- backend/server.js
+++ backend/server.js
@@ -6,9 +6,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-})
+
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
