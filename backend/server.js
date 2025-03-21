e6ad9b7a4dec2665e81db1af45f27971c939ecbf
adityashah2701 <adityashah2701@gmail.com>
2025-03-21 11:40:14 +0530

    Routes - Auth Route file

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,16 @@
 import express from "express";
-
+import authRoutes from "./routes/auth.routes.js";
+import dotenv from "dotenv"
+dotenv.config()
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use("/api/auth",authRoutes)
 
-
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(PORT, () => {
+  console.log("Server is running on port " + PORT);
 });
