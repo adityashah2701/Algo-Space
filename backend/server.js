174f4f72c82f43a6f5f44b6e635f3aceda3c52a3
adityashah2701 <adityashah2701@gmail.com>
2025-03-21 11:49:44 +0530

    Email env changed

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,6 @@ import { connectDB } from "./config/db.js";
 
 const app = express();
 
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -16,5 +15,5 @@ app.use("/api/auth",authRoutes)
 
 app.listen(3000, () => {
   connectDB();
-  console.log("Server is running on port 3000");
+  console.log(`Server running on port ${PORT}`);
 });
