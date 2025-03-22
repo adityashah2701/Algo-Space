44647ea899761386c649e26890d46d6032e0ade9
adityashah2701 <adityashah2701@gmail.com>
2025-03-22 06:48:27 +0530

    final

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -60,13 +60,13 @@ const App = () => {
         <Route path='/login' element={<LoginPage/>}/>
         <Route path='/register' element={<SignupPage/>}/>
     
-        <Route path='/complete-profile' element={<ProfileCompletion/>}/>
-        <Route path='/webrtc' element={<WEBRTC/>}/>
+        <Route path='/complete-profile' element={<ProtectedRoute><ProfileCompletion/></ProtectedRoute>}/>
+        <Route path='/webrtc' element={<ProtectedRoute><WEBRTC/></ProtectedRoute>}/>
 
-        <Route path='/algo-challenge' element={<Algochallenge/>}/>
-        <Route path='/ocr' element={<OCR/>}/>
-        <Route path='/score' element={<Score/>}/>
-        <Route path='/jobs' element={<FindJobs/>}/>
+        <Route path='/algo-challenge' element={<ProtectedRoute><Algochallenge/></ProtectedRoute>}/>
+        <Route path='/ocr' element={<ProtectedRoute><OCR/></ProtectedRoute>}/>
+        <Route path='/score' element={<ProtectedRoute><Score/></ProtectedRoute>}/>
+        <Route path='/jobs' element={<ProtectedRoute><FindJobs/></ProtectedRoute>}/>
 
         
        
--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,10 +1,10 @@
 
-import { useAuthStore } from '@/store/useAuthStore';
+
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 
 const ProtectedRoute = ({ children }) => {
-  const { user } = useAuthStore();
+  const user = JSON.parse(localStorage.getItem('user'));
 
   return user ? children : <Navigate to="/login" replace />;
 };
